17b824442355d587c146f4bae5ae2371fbe6f727
happydilraj <happydilraj1022@gmail.com>
2023-11-25 12:51:59 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import { Test } from './Test';
+import Comic from './comic';
+
 
 function App() {
   return (
     <div className="App">
-      <Test/>
+      <Comic/>
     </div>
   );
 }
--- src/main.js
+++
@@ -1,8 +0,0 @@
-window.onload = function removeLoader() {
-    setTimeout(removeNow, 8000);
-  };
-  
-  function removeNow() {
-    document.getElementById("mainload").style.display = "none";
-  }
-  
